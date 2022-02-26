258cbcb0c074dd821c62169e630f959188d74fb8
putrierikar <putrierikar902@gmail.com>
2022-02-26 16:49:36 +0700

    random words

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
@@ -21,14 +20,6 @@ class MyApp extends StatelessWidget {
 
 class RandomWords extends StatefulWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
@@ -39,12 +30,6 @@ class _RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     //final wordPair = WordPair.random();
     //return Text(wordPair.asPascalCase);
     return Scaffold(
